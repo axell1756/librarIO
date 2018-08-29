a5af112c193f57072b9a12de4ff4b5c7a2fd1dfd
axell1756 <gniteckis@gmail.com>
2018-08-29 10:29:52 +0100

    Initial commit

---
+++ src/main/java/LibrarIO/LibrarIoApplication.java
@@ -0,0 +1,12 @@
+package LibrarIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarIoApplication.class, args);
+	}
+}
